1a15c20aa446c208b9454d072cbaee66e9432263
rasedul <5777245+shaheen2013@users.noreply.github.com>
2025-06-16 13:21:12 +0600

    fix faq left right space trigger issues

--- components/partials/faq/index.tsx
+++ components/partials/faq/index.tsx
@@ -8,7 +8,10 @@ import { faqs } from "./constant";
 
 const Faq = () => {
   return (
-    <section id="faq" className="container grid grid-cols-1 lg:grid-cols-[45%_55%] gap-y-8 gap-x-12 py-12 lg:py-32">
+    <section
+      id="faq"
+      className="container grid grid-cols-1 lg:grid-cols-[45%_55%] gap-y-8 gap-x-12 py-12 lg:py-32"
+    >
       <div className="flex flex-col gap-4">
         <h2 className="text-3xl lg:text-6xl font-semibold text-primary">
           Frequently{" "}
@@ -24,11 +27,11 @@ const Faq = () => {
       <div className="">
         <Accordion type="single" collapsible defaultValue="1">
           {faqs.map((faq) => (
-            <AccordionItem key={faq.id} value={String(faq.id)}>
-              <AccordionTrigger className="data-[state=open]:font-semibold font-medium text-xl lg:text-3xl py-4 cursor-pointer">
+            <AccordionItem key={faq.id} value={String(faq.id)} className="px-0">
+              <AccordionTrigger className="data-[state=open]:font-semibold font-medium text-xl lg:text-3xl py-4 cursor-pointer px-4">
                 {faq.question}
               </AccordionTrigger>
-              <AccordionContent>
+              <AccordionContent className="px-4">
                 <div
                   dangerouslySetInnerHTML={{ __html: faq.ans }}
                   className="text-base lg:text-3xl font-medium text-slate-800"
