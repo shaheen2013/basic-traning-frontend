ec4138c46b6d82dde2b757d96e3e39869e8ef00d
rasedul <5777245+shaheen2013@users.noreply.github.com>
2025-07-10 17:45:01 +0600

    enhanced layouts

--- app/(trainee)/layout.tsx
+++ app/(trainee)/layout.tsx
@@ -6,9 +6,7 @@ export default function CourseLayout({ children }: React.PropsWithChildren) {
     <>
       <CourseHeader />
       {/* Main content area for the course layout */}
-      <main className="min-h-screen overflow-x-hidden font-inter">
-        {children}
-      </main>
+      <main className="font-inter">{children}</main>
       <footer className="bg-gray-800 font-inter">
         <div className="grid lg:grid-cols-2 gap-6 lg:gap-12 container py-4 lg:py-6">
           <p className="text-white text-base lg:text-lg font-normal lg:font-medium">
